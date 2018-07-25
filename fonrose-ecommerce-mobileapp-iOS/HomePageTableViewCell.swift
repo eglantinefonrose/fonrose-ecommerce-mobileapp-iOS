958e4c458d217ca2e7807e93ed43aa30bbdcd90c
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2018-07-25 14:09:01 +0200

    Première simulation (table View avec aspect fit)

---
+++ fonrose-ecommerce-mobileapp-iOS/HomePageTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  HomePageTableViewCell.swift
+//  fonrose-ecommerce-mobileapp-iOS
+//
+//  Created by Eglantine on 25/07/2018.
+//  Copyright © 2018 fonrose. All rights reserved.
+//
+
+import UIKit
+
+class HomePageTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var textOnTheMiddel: UILabel!
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+        
+    
+}
