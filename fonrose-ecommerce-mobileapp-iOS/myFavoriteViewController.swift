73d804c0a87ed5c838b981eb23ff014ace50b195
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2018-08-03 21:54:36 +0200

    +coup de coeur view +measures view +customize view

---
+++ fonrose-ecommerce-mobileapp-iOS/myFavoriteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  myFavoriteViewController.swift
+//  fonrose-ecommerce-mobileapp-iOS
+//
+//  Created by Eglantine on 31/07/2018.
+//  Copyright © 2018 fonrose. All rights reserved.
+//
+
+import UIKit
+
+class myFavoriteViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
