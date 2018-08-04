9d2b59bed9f0e36c7d61a965c36a991a08248458
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2018-08-04 13:47:09 +0200

    Navigation globale terminée

---
+++ fonrose-ecommerce-mobileapp-iOS/MensurationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  myFavoriteViewController.swift
+//  fonrose-ecommerce-mobileapp-iOS
+//
+//  Created by Eglantine on 31/07/2018.
+//  Copyright © 2018 fonrose. All rights reserved.
+//
+
+import UIKit
+
+class MensurationViewController: UIViewController {
+    
+    @IBOutlet weak var armLength: UITextField!
+    @IBOutlet weak var armpitMeasurement: UITextField!
+    @IBOutlet weak var shoulderToPelvisMeasurement: UITextField!
+    @IBOutlet weak var headMeasurement: UITextField!
+    @IBOutlet weak var pelvisToThigh: UITextField!
+    @IBOutlet weak var waistSize: UITextField!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
