1441006bc6eec6d8a9bcf037b40bcd9ec895d045
Eglantine Fonrose <eglantine.fonrose@gmail.com>
2018-08-04 11:03:49 +0200

    [WIP] Ajout de la vue localisation

---
+++ fonrose-ecommerce-mobileapp-iOS/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  fonrose-ecommerce-mobileapp-iOS
+//
+//  Created by Eglantine on 04/08/2018.
+//  Copyright © 2018 fonrose. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation 
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+   
+}
